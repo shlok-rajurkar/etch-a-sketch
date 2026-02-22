eacb0fece0636a40f994ae38f6cb4803eae1b480
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2026-02-21 23:18:18 -0800

    Add hover->color property

--- eas.js
+++ eas.js
@@ -1,16 +1,20 @@
-function createGrid(gridN) {
+function main(gridSize) {
     const gridContainer = document.getElementById("grid-container");
-
-    for (let i = 0; i < gridN; i++) {
+    for (let i = 0; i < gridSize; i++) {
         let gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
-        for (let j = 0; j < gridN; j++) {
+        for (let j = 0; j < gridSize; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             gridRow.appendChild(gridSquare);
         }
         gridContainer.appendChild(gridRow);
     }
-}
-createGrid(16);
 
+    gridContainer.addEventListener("mouseover", (event) => {
+        let target = event.target;
+        target.classList.add("traversed");
+    })
+}
+
+main(16);
